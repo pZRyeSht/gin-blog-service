6a2c437461d1977b0426518348559e26d804f93b
xianjiechen <7426427432@qq.com>
2021-01-31 14:38:32 +0800

    feat: db init

--- scripts/init_db_local.sh
+++ scripts/init_db_local.sh
@@ -2,6 +2,6 @@
 
 ROOT=`pwd`
 
-go run $ROOT/cmd/mysql-db-migrate/main.go -conf $ROOT/config/ -do drop
-go run $ROOT/cmd/mysql-db-migrate/main.go -conf $ROOT/config/ -do create
-go run $ROOT/cmd/mysql-db-migrate/main.go -conf $ROOT/config/ -do migrate
+go run $ROOT/cmd/mysql-db-migrate/main.go -conf $ROOT/configs/ -do drop
+go run $ROOT/cmd/mysql-db-migrate/main.go -conf $ROOT/configs/ -do create
+go run $ROOT/cmd/mysql-db-migrate/main.go -conf $ROOT/configs/ -do migrate
